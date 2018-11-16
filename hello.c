57557d40edb09e2105f3a928718803993db70f76
Kim HaeJoo <mrhjkim@uangel.com>
2018-11-16 11:01:03 +0900

    no initialized warning

--- hello.c
+++ hello.c
@@ -1,8 +1,10 @@
 #include <stdio.h>
 
 int main(void) {
+  int i;
   printf( "Hello, World");
   printf( "Hello, Jenkins");
+  printf( "warning for no initialized use %d", i);
 
   printf( "I have successfully built and run");
   return 0;
