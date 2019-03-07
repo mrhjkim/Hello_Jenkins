c4f2286b67963bec0772194eb986699280a421b8
Kim HaeJoo <mrhjkim@uangel.com>
2019-03-07 20:01:40 +0900

    log authorname and files of commit 5

--- Jenkinsfile.dailycheck.groovy
+++ Jenkinsfile.dailycheck.groovy
@@ -10,15 +10,15 @@ node {
     stage('CommitCheck') {
 		changedFiles = []
 		for (changeLogSet in currentBuild.changeSets) {
+			changedFilesInCommit = []
 			for (entry in changeLogSet.getItems()) { // for each commit in the detected changes
-				changedFilesInCommit = []
 				for (file in entry.getAffectedFiles()) {
 					changedFiles.add(file.getPath()) // add changed file to list
 					changedFilesInCommit.add(file.getPath()) // add changed file to list
 				}
-				authorName = entry.getAuthorName()
-				echo "user:${authorName}, files:${changedFilesInCommit}"
 			}
+			authorName = changeLogSet.getAuthorName()
+			echo "user:${authorName}, files:${changedFilesInCommit}"
 		}
 		echo "changed files : ${changedFiles}"
     }
